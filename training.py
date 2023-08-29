430a58ae11cb7e7b41f11615a384f01b47f109ef
navneet <navneetsharma2302@gmail.com>
2023-08-29 09:15:53 +0530

    first

---
+++ training.py
@@ -0,0 +1,32 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+
+from tensorflow import keras
+from keras.models import Model,Sequential
+from keras.layers import Dense,Activation, Dropout
+from keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+
+intents = json.loads(open('intents.json').read())
+
+words =[]
+classes =[]
+documents= []
+ignore_lettters = ['?','!','.',',']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list),intent['tag'])
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)                      
